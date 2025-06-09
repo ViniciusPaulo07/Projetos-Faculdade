b4410b435f16853a945f3c2e3635891d6062466b
Vinicius Paulo <viniciusaop767@gmail.com>
2025-06-09 20:49:44 -0300

    Adicionei comentarios

--- Aula1_Carmino/src/Aula1.java
+++ Aula1_Carmino/src/Aula1.java
@@ -1,14 +1,28 @@
+//Importa a classe Scanner, que serve para ler o que o usuário digita.
+
 import java.util.Scanner;
 
+//Cria uma classe chamada Aula1
 public class Aula1 {
+    //Esse é o ponto de entrada.
+    //O Java sempre começa executando o main. Sem isso , nada roda.
     public static void main(String[] args) {
+        //Cria uma váriavel chamada numero que vai guardar um valor inteiro
         int numero;
+        /*Cria um objeto chamado entrada que usa Scanner para ler o
+         que o usuário digitar*/
         Scanner entrada = new Scanner(System.in);
+        //Aqui o programa espera o usuário digitar, para guardalo na variável numero
         numero = entrada.nextInt();
+        //Se o numero digitado for igual a zero
         if (numero == 0) {
+            //Mostra isso
             System.out.println("O numero e Zero");
+            //Senão se o numero for maior ou igual a 1
         } else if (numero >= 1) {
+            //Mostra isso
             System.out.println("O numero e Positivo");
+            //Senão mostra isso
         } else {
             System.out.println("O numero e Negativo");
         }
