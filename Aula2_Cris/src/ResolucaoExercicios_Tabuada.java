4ddfcdee72988c74a728e79207cbf709f01a1ad2
ViniciusPaulo <viniciuspaulo0703@gmail.com>
2025-07-30 19:31:32 -0300

    Exercicios resolvidos

---
+++ Aula2_Cris/src/ResolucaoExercicios_Tabuada.java
@@ -0,0 +1,12 @@
+public class ResolucaoExercicios_Tabuada {
+
+    public static void main(String[] args) {
+        int res,num1;
+        num1=5;
+
+        for(int i=0;i<=10;i++){
+            res=num1*i;
+            System.out.println("O resultado de " + num1 +"x" + i + "=" + res);
+        }
+    }
+}
