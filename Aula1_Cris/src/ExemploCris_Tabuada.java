4ade946362219e7a0b1c7e1578c7b2d35b7f8cc2
ViniciusPaulo <viniciuspaulo0703@gmail.com>
2025-07-30 19:20:40 -0300

    Exemplos da Professora dos exercicios

---
+++ Aula1_Cris/src/ExemploCris_Tabuada.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+
+public class ExemploCris_Tabuada {
+    public static void main(String[]args){
+        String a,b;
+        float x,y,res;
+        a= JOptionPane.showInputDialog("DIgite o primeiro numero");
+        x= Float.parseFloat(a);
+        b= JOptionPane.showInputDialog("Digite o segundo numero");
+        y= Float.parseFloat(b);
+        res=(x+y);
+        System.out.print(res);
+        JOptionPane.showMessageDialog(null, "Resultado da soma:" + res);
+        res=(x-y);
+        JOptionPane.showMessageDialog(null, "Resultado da subtracacao:" + res);
+        res=(x*y);
+        JOptionPane.showMessageDialog(null, "Resultado da multiplicacao:" + res);
+        res=(x/y);
+        JOptionPane.showMessageDialog(null, "Resultado da divisao:" + res);
+
+    }
+}
