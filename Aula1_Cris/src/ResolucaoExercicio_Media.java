b997e23e88c353e39dc716c18aa36d085b2b8610
ViniciusPaulo <viniciuspaulo0703@gmail.com>
2025-07-30 19:11:30 -0300

    Exercicio media

---
+++ Aula1_Cris/src/ResolucaoExercicio_Media.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ResolucaoExercicio_Media {
+    public static void main(String[]args){
+        Scanner scanner=new Scanner(System.in);
+
+        System.out.print("Digite quantos km foram percorridos:");
+        double distancia=scanner.nextDouble();
+        System.out.print("Digite quantos litros de combustivel foram gasto:");
+        double combustivel=scanner.nextDouble();
+
+        double consumoMedio=distancia/combustivel;
+
+        System.out.println("O consumo medio do automovel e:"+consumoMedio+"Km/litros");
+
+        scanner.close();
+    }
+}
