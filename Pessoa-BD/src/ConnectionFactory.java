32577a6210e8e5b61120167cc65e52c2973049fc
ViniciusPaulo <viniciuspaulo0703@gmail.com>
2025-08-01 09:45:30 -0300

    Conexao com banco de dados

---
+++ Pessoa-BD/src/ConnectionFactory.java
@@ -0,0 +1,27 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/db_pessoas" +
+            "?useSSL=false" +
+            "&allowPublicKeyRetrieval=true" +
+            "&serverTimezone=UTC" +
+            "&autoReconnect=true" +
+            "&socketTimeout=3000";
+    private static final String USER = "root";
+    private static final String PASSWORD = "@Vipp0703";
+
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Driver JDBC não encontrado", e);
+        }
+    }
+
+    public Connection obtemConexao() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
+
