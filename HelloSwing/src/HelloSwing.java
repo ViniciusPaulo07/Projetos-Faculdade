f73b4560c47c017a3305859430791f66da7f0cf8
ViniciusPaulo <viniciuspaulo0703@gmail.com>
2025-08-01 09:27:56 -0300

    Hello Swing

---
+++ HelloSwing/src/HelloSwing.java
@@ -0,0 +1,23 @@
+import java.awt.Container;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
+
+public class HelloSwing {
+    public static void criarTela() {
+        JFrame tela=new JFrame("Hello, Swing!!!");
+        tela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel helloSwingLabel=new JLabel("Hello, Swing!!!!!!!");
+        Container painelDeConteudo=tela.getContentPane();
+        painelDeConteudo.add(helloSwingLabel);
+
+        tela.pack();
+        tela.setVisible(true);
+    }
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(()->{
+            criarTela();
+        });
+    }
+}
